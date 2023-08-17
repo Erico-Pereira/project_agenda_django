89a57205fe3d6fb6269bcfcce5ad46ab055621ff
Erico Pereira <erico_perera@hotmail.com>
2023-08-16 22:21:50 -0400

    Changing the admin.py settings

--- contact/admin.py
+++ contact/admin.py
@@ -6,4 +6,8 @@ from contact import models
 
 @admin.register(models.Contact)
 class Contact_admin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    ordering = 'id',
+    # list_filter = 'created_ate',
+    search_fields = 'first_name',
+    list_per_page = 1
